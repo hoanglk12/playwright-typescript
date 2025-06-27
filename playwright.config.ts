7d2cf38da2a934d4928a56c8788381e7fa4f07a3
Hoang Pham <hoanglk12@gmail.com>
2025-06-27 21:58:48 +0700

    handle upload - drag and drop

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig, devices } from '@playwright/test';
 import { getEnvironment } from './src/config/environment';
 
+
 // Get environment configuration
 const env = getEnvironment();
 
